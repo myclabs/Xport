30933e7275efbcfd04b9be42e6a43b183d0cb3af
valentin-mcs <eu4a12n>
2013-07-05 11:46:00 +0200

    Ajout des fonctions dans les foreach

--- src/Xport/SpreadsheetModel/Parser/ForEachParser.php
+++ src/Xport/SpreadsheetModel/Parser/ForEachParser.php
@@ -2,6 +2,8 @@
 
 namespace Xport\SpreadsheetModel\Parser;
 
+use Symfony\Component\PropertyAccess\PropertyAccess;
+use Symfony\Component\PropertyAccess\PropertyAccessor;
 use Xport\SpreadsheetModel\Parser\ParsingException;
 
 /**
@@ -12,35 +14,55 @@ use Xport\SpreadsheetModel\Parser\ParsingException;
 class ForEachParser
 {
     /**
+     * @var String
+     */
+    private $partRegEx;
+    /**
+     * @var PropertyAccessor
+     */
+    private $propertyAccessor;
+
+    public function __construct()
+    {
+        $this->partRegEx = '([[:alnum:]]+\([[:alnum:]\.\[\]]+\)|[[:alnum:]\.\[\]]+)';
+        $this->propertyAccessor = PropertyAccess::createPropertyAccessor();
+    }
+
+    /**
      * Parse a foreach expression.
      *
      * The expression has the form: 'foo as bar', where foo is an array and bar the value.
      *
      * @param string $str foreach expression
+     * @param scope $scope
      *
      * @throws ParsingException
-     * @return array Keys are 'array' and 'value'
+     * @return Scope[]
      */
-    public function parse($str)
+    public function parse($str, $scope)
     {
         $result = $this->parseWithKey($str);
 
         if (is_array($result)) {
-            return $result;
+            return $this->getSubScopes($result, $scope);
         }
 
         $result = $this->parseWithoutKey($str);
 
         if (is_array($result)) {
-            return $result;
+            return $this->getSubScopes($result, $scope);
         }
 
         throw new ParsingException("Error while parsing '$str', should be in the form 'array as value' or 'array as key => value'");
     }
 
+    /**
+     * @param string $str
+     * @return array|null Keys are 'array' and 'value'
+     */
     public function parseWithKey($str)
     {
-        $result = preg_match('/^\s*([[:alnum:]\.\[\]]+)\s*as\s*([[:alnum:]\.\[\]]+)\s*=>\s*([[:alnum:]\.\[\]]+)\s*$/', $str, $matches);
+        $result = preg_match('/^\s*'.$this->partRegEx.'\s*as\s*'.$this->partRegEx.'\s*=>\s*'.$this->partRegEx.'\s*$/', $str, $matches);
 
         if ($result !== 1) {
             return null;
@@ -53,9 +75,13 @@ class ForEachParser
         ];
     }
 
+    /**
+     * @param string $str
+     * @return array|null Keys are 'array' and 'value'
+     */
     public function parseWithoutKey($str)
     {
-        $result = preg_match('/^\s*([[:alnum:]\.\[\]]+)\s*as\s*([[:alnum:]\.\[\]]+)\s*$/', $str, $matches);
+        $result = preg_match('/^\s*'.$this->partRegEx.'\s*as\s*'.$this->partRegEx.'\s*$/', $str, $matches);
 
         if ($result !== 1) {
             return null;
@@ -65,5 +91,71 @@ class ForEachParser
             'array' => $matches[1],
             'value' => $matches[2],
         ];
+    }
+
+    /**
+     * @param string $str
+     * @return array|null Keys are 'array' and 'value'
+     */
+    public function parseFunction($str)
+    {
+        $result = preg_match('/^\s*([[:alnum:]]+)\(([[:alnum:]\.\[\]]+)\)\s*$/', $str, $matches);
+
+        if ($result !== 1) {
+            return null;
+        }
+
+        return [
+            'functionName' => $matches[1],
+            'parameter' => $matches[2],
+        ];
+    }
+
+    /**
+     * @param array $result
+     * @param Scope $scope
+     * @return Scope[]
+     */
+    public function getSubScopes($result, Scope $scope)
+    {
+        $subScopes = [];
+
+        $resultArray = $this->parseFunction($result['array']);
+
+        if (is_array($resultArray)) {
+            $function = $scope->getFunction($resultArray['functionName']);
+            $array = $function($this->propertyAccessor->getValue($scope, $resultArray['parameter']));
+        } else {
+            $array = $this->propertyAccessor->getValue($scope, $result['array']);
+        }
+
+        foreach ($array as $key => $value) {
+            // New sub-scope
+            $subScope = new Scope($scope);
+
+            $resultValue = $this->parseFunction($result['value']);
+
+            if (is_array($resultValue)) {
+                $function = $scope->getFunction($resultValue['functionName']);
+                $subScope->bind($resultValue['parameter'], $function($value));
+            } else {
+                $subScope->bind($result['value'], $value);
+            }
+
+            if (isset($result['key'])) {
+                $resultKey = $this->parseFunction($result['key']);
+
+                if (is_array($resultKey)) {
+                    $function = $scope->getFunction($resultKey['functionName']);
+                    $subScope->bind($resultKey['parameter'], $function($key));
+                } else {
+                    $subScope->bind($result['key'], $key);
+                }
+            }
+
+            $subScopes[] = $subScope;
+        }
+
+        return $subScopes;
     }
 }
--- tests/XportTest/SpreadsheetModel/Parser/ForEachParserTest.php
+++ tests/XportTest/SpreadsheetModel/Parser/ForEachParserTest.php
@@ -3,68 +3,139 @@
 namespace XportTest\SpreadsheetModel\Parser;
 
 use Xport\SpreadsheetModel\Parser\ForEachParser;
+use Xport\SpreadsheetModel\Parser\Scope;
 
 class ForEachParserTest extends \PHPUnit_Framework_TestCase
 {
     public function testSimple()
     {
+        $scope = new Scope();
+        $scope->bind('foo', ['barValue0', 'barValue1']);
+
         $parser = new ForEachParser();
 
-        $result = $parser->parse('foo as bar');
+        $subScopes = $parser->parse('foo as bar', $scope);
 
-        $this->assertArrayHasKey('array', $result);
-        $this->assertEquals('foo', $result['array']);
+        $this->assertCount(2, $subScopes);
 
-        $this->assertArrayHasKey('value', $result);
-        $this->assertEquals('bar', $result['value']);
+        foreach ($subScopes as $i => $subScope) {
+            $this->assertInstanceOf('Xport\SpreadsheetModel\Parser\Scope', $subScope);
+            $this->assertEquals('barValue'.$i, $subScope->get('bar'));
+        }
     }
 
     public function testWithKey1()
     {
+        $scope = new Scope();
+        $scope->bind('foo', ['bimIndex0' => 'barValue0', 'bimIndex1' => 'barValue1']);
+
         $parser = new ForEachParser();
 
-        $result = $parser->parse('foo as bim => bar');
+        $subScopes = $parser->parse('foo as bim => bar', $scope);
 
-        $this->assertArrayHasKey('array', $result);
-        $this->assertEquals('foo', $result['array']);
+        $this->assertCount(2, $subScopes);
 
-        $this->assertArrayHasKey('value', $result);
-        $this->assertEquals('bar', $result['value']);
-
-        $this->assertArrayHasKey('key', $result);
-        $this->assertEquals('bim', $result['key']);
+        foreach ($subScopes as $i => $subScope) {
+            $this->assertInstanceOf('Xport\SpreadsheetModel\Parser\Scope', $subScope);
+            $this->assertEquals('bimIndex'.$i, $subScope->get('bim'));
+            $this->assertEquals('barValue'.$i, $subScope->get('bar'));
+        }
     }
 
     public function testWithKey2()
     {
+        $scope = new Scope();
+        $scope->bind('foo', ['bimIndex0' => 'barValue0', 'bimIndex1' => 'barValue1']);
+
         $parser = new ForEachParser();
 
-        $result = $parser->parse('foo as bim=>bar');
+        $subScopes = $parser->parse('foo as bim=>bar', $scope);
 
-        $this->assertArrayHasKey('array', $result);
-        $this->assertEquals('foo', $result['array']);
+        $this->assertCount(2, $subScopes);
 
-        $this->assertArrayHasKey('value', $result);
-        $this->assertEquals('bar', $result['value']);
-
-        $this->assertArrayHasKey('key', $result);
-        $this->assertEquals('bim', $result['key']);
+        foreach ($subScopes as $i => $subScope) {
+            $this->assertInstanceOf('Xport\SpreadsheetModel\Parser\Scope', $subScope);
+            $this->assertEquals('bimIndex'.$i, $subScope->get('bim'));
+            $this->assertEquals('barValue'.$i, $subScope->get('bar'));
+        }
     }
 
     public function testWithKey3()
     {
+        $foo = new \stdClass();
+        $foo->blah = ['bleh' => ['bimIndex0' => 'barValue0', 'bimIndex1' => 'barValue1']];
+        $scope = new Scope();
+        $scope->bind('foo', $foo);
+
         $parser = new ForEachParser();
 
-        $result = $parser->parse('  foo.blah[bleh]  as      bim  =>    bar ');
+        $subScopes = $parser->parse('  foo.blah[bleh]  as      bim  =>    bar ', $scope);
 
-        $this->assertArrayHasKey('array', $result);
-        $this->assertEquals('foo.blah[bleh]', $result['array']);
+        $this->assertCount(2, $subScopes);
 
-        $this->assertArrayHasKey('value', $result);
-        $this->assertEquals('bar', $result['value']);
+        foreach ($subScopes as $i => $subScope) {
+            $this->assertInstanceOf('Xport\SpreadsheetModel\Parser\Scope', $subScope);
+            $this->assertEquals('bimIndex'.$i, $subScope->get('bim'));
+            $this->assertEquals('barValue'.$i, $subScope->get('bar'));
+        }
+    }
 
-        $this->assertArrayHasKey('key', $result);
-        $this->assertEquals('bim', $result['key']);
+    public function testWithFunction1()
+    {
+        $scope = new Scope();
+        $scope->bind('foo', ['bimIndex0' => 'barValue0', 'bimIndex1' => 'barValue1']);
+        $scope->bindFunction('foo', function ($parameter) { return ucfirst($parameter); });
+
+        $parser = new ForEachParser();
+
+        $subScopes = $parser->parse('foo as bim => foo(bar)', $scope);
+
+        $this->assertCount(2, $subScopes);
+
+        foreach ($subScopes as $i => $subScope) {
+            $this->assertInstanceOf('Xport\SpreadsheetModel\Parser\Scope', $subScope);
+            $this->assertEquals('bimIndex'.$i, $subScope->get('bim'));
+            $this->assertEquals('BarValue'.$i, $subScope->get('bar'));
+        }
+    }
+
+    public function testWithFunction2()
+    {
+        $scope = new Scope();
+        $scope->bind('foo', ['bimIndex0' => 'barValue0', 'bimIndex1' => 'barValue1']);
+        $scope->bindFunction('foo', function ($parameter) { return ucfirst($parameter); });
+
+        $parser = new ForEachParser();
+
+        $subScopes = $parser->parse('foo as foo(bim) => foo(bar)', $scope);
+
+        $this->assertCount(2, $subScopes);
+
+        foreach ($subScopes as $i => $subScope) {
+            $this->assertInstanceOf('Xport\SpreadsheetModel\Parser\Scope', $subScope);
+            $this->assertEquals('BimIndex'.$i, $subScope->get('bim'));
+            $this->assertEquals('BarValue'.$i, $subScope->get('bar'));
+        }
+    }
+
+    public function testWithFunction3()
+    {
+        $scope = new Scope();
+        $scope->bind('foo', ['0', '1']);
+        $scope->bindFunction('blah', function ($parameters) { $a=[]; foreach($parameters as $i) {$a['bimIndex'.$i] = 'barValue'.$i;} return $a; });
+        $scope->bindFunction('bleh', function ($parameter) { return ucfirst($parameter); });
+
+        $parser = new ForEachParser();
+
+        $subScopes = $parser->parse('blah(foo) as bleh(bim) => bleh(bar)', $scope);
+
+        $this->assertCount(2, $subScopes);
+
+        foreach ($subScopes as $i => $subScope) {
+            $this->assertInstanceOf('Xport\SpreadsheetModel\Parser\Scope', $subScope);
+            $this->assertEquals('BimIndex'.$i, $subScope->get('bim'));
+            $this->assertEquals('BarValue'.$i, $subScope->get('bar'));
+        }
     }
 
     /**
@@ -73,7 +144,7 @@ class ForEachParserTest extends \PHPUnit_Framework_TestCase
     public function testInvalidString1()
     {
         $parser = new ForEachParser();
-        $parser->parse('');
+        $parser->parse('', new Scope());
     }
 
     /**
@@ -82,7 +153,7 @@ class ForEachParserTest extends \PHPUnit_Framework_TestCase
     public function testInvalidString2()
     {
         $parser = new ForEachParser();
-        $parser->parse('foobar');
+        $parser->parse('foobar', new Scope());
     }
 
     /**
@@ -91,7 +162,7 @@ class ForEachParserTest extends \PHPUnit_Framework_TestCase
     public function testInvalidString3()
     {
         $parser = new ForEachParser();
-        $parser->parse('foo as bar as test');
+        $parser->parse('foo as bar as test', new Scope());
     }
 
     /**
@@ -100,7 +171,7 @@ class ForEachParserTest extends \PHPUnit_Framework_TestCase
     public function testInvalidString4()
     {
         $parser = new ForEachParser();
-        $parser->parse('foo => bar as test');
+        $parser->parse('foo => bar as test', new Scope());
     }
 
     /**
@@ -109,7 +180,7 @@ class ForEachParserTest extends \PHPUnit_Framework_TestCase
     public function testInvalidString5()
     {
         $parser = new ForEachParser();
-        $parser->parse('foo as');
+        $parser->parse('foo as', new Scope());
     }
 
     /**
@@ -118,7 +189,7 @@ class ForEachParserTest extends \PHPUnit_Framework_TestCase
     public function testInvalidString6()
     {
         $parser = new ForEachParser();
-        $parser->parse('foo as bar =>');
+        $parser->parse('foo as bar =>', new Scope());
     }
 
     /**
@@ -127,6 +198,6 @@ class ForEachParserTest extends \PHPUnit_Framework_TestCase
     public function testInvalidString7()
     {
         $parser = new ForEachParser();
-        $parser->parse('foo bar');
+        $parser->parse('foo bar', new Scope());
     }
 }
