420de1a6fe5ed9db3595435d4f39957f5f053c19
Matthieu Napoli <matthieu@mnapoli.fr>
2013-10-03 17:26:01 +0200

    Refactored PHPExcelExporter a bit

--- src/Xport/Spreadsheet/Exporter/PHPExcelExporter.php
+++ src/Xport/Spreadsheet/Exporter/PHPExcelExporter.php
@@ -3,14 +3,18 @@
 namespace Xport\Spreadsheet\Exporter;
 
 use PHPExcel;
+use PHPExcel_Cell;
+use PHPExcel_Worksheet;
 use PHPExcel_Writer_Excel2007;
 use PHPExcel_Writer_IWriter;
 use Xport\Spreadsheet\Model\Document;
+use Xport\Spreadsheet\Model\Sheet;
 
 /**
  * Exports an Spreadsheet model to an Excel file.
  *
  * @author Matthieu Napoli <matthieu@mnapoli.fr>
+ * @author valentin-mcs <valentin.claras@myc-sense.com>
  */
 class PHPExcelExporter
 {
@@ -31,141 +35,8 @@ class PHPExcelExporter
             }
 
             $phpExcelSheet = $phpExcelModel->getSheet($sheetIndex);
-            if ($sheet->getLabel()) {
-                $phpExcelSheet->setTitle(substr($sheet->getLabel(), 0, 31));
-            }
 
-            $lineOffset = 1;
-            $lastCol = 0;
-
-            // Tables
-            foreach ($sheet->getTables() as $table) {
-                $tableStartingLineIndex = $lineOffset;
-                $tableStartingColumnIndex = \PHPExcel_Cell::stringFromColumnIndex(0);
-                $tableEndingColumnIndex = \PHPExcel_Cell::stringFromColumnIndex(count($table->getColumns()) - 1);
-
-                if ($table->getLabel() !== null) {
-                    $phpExcelSheet->setCellValueByColumnAndRow(
-                        0,
-                        $lineOffset,
-                        $table->getLabel()
-                    );
-                    if (count($table->getColumns()) > 1) {
-                        $phpExcelSheet->mergeCells(
-                            $tableStartingColumnIndex.$lineOffset.
-                            ':'.
-                            $tableEndingColumnIndex.$lineOffset
-                        );
-                    }
-                    $lineOffset ++;
-                }
-
-                // Header.
-                if ($table->displayColumnsLabel()) {
-                    $lineOffset++;
-                }
-
-                // Columns
-                foreach ($table->getColumns() as $columnIndex => $column) {
-                    if ($lastCol < $columnIndex) {
-                        $lastCol = $columnIndex;
-                    }
-
-                    // Column header
-                    if ($table->displayColumnsLabel()) {
-                        $phpExcelSheet->setCellValueByColumnAndRow($columnIndex, ($lineOffset - 1), $column->getLabel());
-                    }
-
-                    // Lines
-                    foreach ($table->getLines() as $lineIndex => $line) {
-                        // Cell
-                        $cell = $table->getCell($line, $column);
-                        if ($cell !== null) {
-                            $phpExcelSheet->setCellValueByColumnAndRow(
-                                $columnIndex,
-                                $lineOffset + $lineIndex,
-                                $cell->getContent()
-                            );
-                        }
-
-                    }
-                }
-                $lineOffset += count($table->getLines());
-
-                // Style.
-                //@todo move to a StyleBuilder.
-
-                // Add an empty line after each content.
-                $lineOffset ++;
-
-                // Border and italic for Table label.
-                if ($table->getLabel() !== null) {
-                    $cellCoordinates = $tableStartingColumnIndex . $tableStartingLineIndex;
-                    if (count($table->getColumns()) > 1) {
-                        $cellCoordinates .= ':' . $tableEndingColumnIndex . $tableStartingLineIndex;
-                    }
-
-                    $phpExcelSheet->getStyle($cellCoordinates)->applyFromArray(
-                        [
-                            'borders' => [
-                                'allborders' => [
-                                    'style' => \PHPExcel_Style_Border::BORDER_HAIR
-                                ]
-                            ],
-                            'alignment' => [
-                                'horizontal' => \PHPExcel_Style_Alignment::HORIZONTAL_CENTER,
-                            ],
-                            'font' => [
-                                'italic' => true
-                            ]
-                        ]
-                    );
-
-                    $tableStartingLineIndex++;
-                }
-
-                // Border and bold for column header.
-                if ($table->displayColumnsLabel()) {
-                    $cellCoordinates = $tableStartingColumnIndex . $tableStartingLineIndex .
-                        ':' . $tableEndingColumnIndex . $tableStartingLineIndex;
-                    $phpExcelSheet->getStyle($cellCoordinates)->applyFromArray(
-                        [
-                            'borders' => [
-                                'allborders' => [
-                                    'style' => \PHPExcel_Style_Border::BORDER_HAIR
-                                ]
-                            ],
-                            'alignment' => [
-                                'horizontal' => \PHPExcel_Style_Alignment::HORIZONTAL_CENTER,
-                            ],
-                            'font' => [
-                                'bold' => true
-                            ]
-                        ]
-                    );
-                    $tableStartingLineIndex++;
-                }
-
-                // Border around each lines.
-                foreach ($table->getLines() as $lineIndex => $line) {
-                    $lineNumber = $tableStartingLineIndex + $lineIndex;
-                    $cellCoordinates = $tableStartingColumnIndex . $lineNumber . ':' . $tableEndingColumnIndex . $lineNumber;
-                    $phpExcelSheet->getStyle($cellCoordinates)->applyFromArray(
-                        [
-                            'borders' => [
-                                'allborders' => [
-                                    'style' => \PHPExcel_Style_Border::BORDER_HAIR
-                                ]
-                            ],
-                        ]
-                    );
-                }
-            }
-
-            // Set automatic calculation for col's width.
-            for ($columnIndex = 0; $columnIndex <= $lastCol; $columnIndex++) {
-                $phpExcelSheet->getColumnDimension(\PHPExcel_Cell::stringFromColumnIndex($columnIndex))->setAutoSize(true);
-            }
+            $this->processSheet($sheet, $phpExcelSheet);
         }
 
         if (count($model->getSheets()) > 0) {
@@ -173,6 +44,140 @@ class PHPExcelExporter
         }
 
         $this->writeToFile($phpExcelModel, $targetFile, $writer);
+    }
+
+    private function processSheet(Sheet $sheet, PHPExcel_Worksheet $phpExcelSheet)
+    {
+        if ($sheet->getLabel()) {
+            $phpExcelSheet->setTitle(substr($sheet->getLabel(), 0, 31));
+        }
+
+        $lineOffset = 1;
+        $lastCol = 0;
+
+        // Tables
+        foreach ($sheet->getTables() as $table) {
+            $tableStartingLineIndex = $lineOffset;
+            $tableStartingColumnIndex = PHPExcel_Cell::stringFromColumnIndex(0);
+            $tableEndingColumnIndex = PHPExcel_Cell::stringFromColumnIndex(count($table->getColumns()) - 1);
+
+            if ($table->getLabel() !== null) {
+                $phpExcelSheet->setCellValueByColumnAndRow(0, $lineOffset, $table->getLabel());
+                if (count($table->getColumns()) > 1) {
+                    $phpExcelSheet->mergeCells(
+                        $tableStartingColumnIndex . $lineOffset . ':'
+                        . $tableEndingColumnIndex . $lineOffset
+                    );
+                }
+                $lineOffset ++;
+            }
+
+            // Header.
+            if ($table->displayColumnsLabel()) {
+                $lineOffset++;
+            }
+
+            // Columns
+            foreach ($table->getColumns() as $columnIndex => $column) {
+                if ($lastCol < $columnIndex) {
+                    $lastCol = $columnIndex;
+                }
+
+                // Column header
+                if ($table->displayColumnsLabel()) {
+                    $phpExcelSheet->setCellValueByColumnAndRow($columnIndex, ($lineOffset - 1), $column->getLabel());
+                }
+
+                // Lines
+                foreach ($table->getLines() as $lineIndex => $line) {
+                    // Cell
+                    $cell = $table->getCell($line, $column);
+                    if ($cell !== null) {
+                        $phpExcelSheet->setCellValueByColumnAndRow(
+                            $columnIndex,
+                            $lineOffset + $lineIndex,
+                            $cell->getContent()
+                        );
+                    }
+
+                }
+            }
+            $lineOffset += count($table->getLines());
+
+            // Style.
+            //@todo move to a StyleBuilder.
+
+            // Add an empty line after each content.
+            $lineOffset ++;
+
+            // Border and italic for Table label.
+            if ($table->getLabel() !== null) {
+                $cellCoordinates = $tableStartingColumnIndex . $tableStartingLineIndex;
+                if (count($table->getColumns()) > 1) {
+                    $cellCoordinates .= ':' . $tableEndingColumnIndex . $tableStartingLineIndex;
+                }
+
+                $phpExcelSheet->getStyle($cellCoordinates)->applyFromArray(
+                    [
+                        'borders' => [
+                            'allborders' => [
+                                'style' => \PHPExcel_Style_Border::BORDER_HAIR
+                            ]
+                        ],
+                        'alignment' => [
+                            'horizontal' => \PHPExcel_Style_Alignment::HORIZONTAL_CENTER,
+                        ],
+                        'font' => [
+                            'italic' => true
+                        ]
+                    ]
+                );
+
+                $tableStartingLineIndex++;
+            }
+
+            // Border and bold for column header.
+            if ($table->displayColumnsLabel()) {
+                $cellCoordinates = $tableStartingColumnIndex . $tableStartingLineIndex .
+                    ':' . $tableEndingColumnIndex . $tableStartingLineIndex;
+                $phpExcelSheet->getStyle($cellCoordinates)->applyFromArray(
+                    [
+                        'borders' => [
+                            'allborders' => [
+                                'style' => \PHPExcel_Style_Border::BORDER_HAIR
+                            ]
+                        ],
+                        'alignment' => [
+                            'horizontal' => \PHPExcel_Style_Alignment::HORIZONTAL_CENTER,
+                        ],
+                        'font' => [
+                            'bold' => true
+                        ]
+                    ]
+                );
+                $tableStartingLineIndex++;
+            }
+
+            // Border around each lines.
+            foreach ($table->getLines() as $lineIndex => $line) {
+                $lineNumber = $tableStartingLineIndex + $lineIndex;
+                $cellCoordinates = $tableStartingColumnIndex . $lineNumber . ':' . $tableEndingColumnIndex . $lineNumber;
+                $phpExcelSheet->getStyle($cellCoordinates)->applyFromArray(
+                    [
+                        'borders' => [
+                            'allborders' => [
+                                'style' => \PHPExcel_Style_Border::BORDER_HAIR
+                            ]
+                        ],
+                    ]
+                );
+            }
+        }
+
+        // Set automatic calculation for col's width.
+        for ($columnIndex = 0; $columnIndex <= $lastCol; $columnIndex++) {
+            $phpExcelSheet->getColumnDimension(PHPExcel_Cell::stringFromColumnIndex($columnIndex))->setAutoSize(true);
+        }
     }
 
     private function writeToFile(PHPExcel $phpExcelModel, $targetFile, PHPExcel_Writer_IWriter $writer = null)
